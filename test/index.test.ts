ffdc9badf5a244d61f09cbf9785d0c49b3146eb3
Sébastien Galoux <sga@nsi-sa.be>
2019-09-08 22:40:19 +0200

    Add first test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,12 @@
-import { testWorkingJsEnv } from "../src";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useTableState } from "../src/useTableState";
 
-test("working", () => {
-  const data = testWorkingJsEnv();
-  expect(data).toBeTruthy();
+test("Should store basic grid data", () => {
+  const { result } = renderHook(() =>
+    useTableState({
+      data: [{ a: 1 }]
+    })
+  );
+
+  expect(result.current.data.length).toBe(1);
 });
